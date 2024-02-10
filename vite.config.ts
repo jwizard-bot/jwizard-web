9d254854b3e0596be0c8a50e9a22145bbca971b9
Milosz08 <gilgamilosz451@gmail.com>
2024-02-10 12:27:00 +0100

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
 import { defineConfig } from 'vite';
+import { URL, fileURLToPath } from 'url';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
 });
