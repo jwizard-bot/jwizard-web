a5efc41fc9568caeb518d56ed38383bc2f7adc0f
Milosz08 <gilgamilosz451@gmail.com>
2024-02-16 12:29:20 +0100

    rewrite vite config for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,42 @@
  * Copyright (c) 2024 by JWizard
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import dotenv from 'dotenv';
 import { URL, fileURLToPath } from 'url';
 import vue from '@vitejs/plugin-vue';
 
-dotenv.config();
-
-const { hostname, port } = new URL(process.env.BASE_PATH!);
-
-export default defineConfig({
-  server: {
-    host: hostname,
-    port: Number(port),
-  },
-  plugins: [
-    vue(),
-    createHtmlPlugin({
-      entry: 'src/main.ts',
-      template: '/index.html',
-      inject: {
-        data: {
-          apiUrl: process.env.API_URL,
-          inviteLink: process.env.INVITE_LINK,
-          orgLink: process.env.ORG_LINK,
-          basePath: process.env.BASE_PATH,
+export default () => {
+  const env = loadEnv('', process.cwd(), 'VITE_');
+  const { hostname, port } = new URL(env.VITE_BASE_PATH);
+  return defineConfig({
+    server: {
+      host: hostname,
+      port: Number(port),
+    },
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        entry: 'src/main.ts',
+        template: '/index.html',
+        minify: process.env.PROD,
+        inject: {
+          data: {
+            apiUrl: env.VITE_API_URL,
+            inviteLink: env.VITE_INVITE_LINK,
+            orgLink: env.VITE_ORG_LINK,
+            basePath: env.VITE_BASE_PATH,
+          },
         },
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
+      }),
     ],
-  },
-});
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      ],
+    },
+  });
+};
