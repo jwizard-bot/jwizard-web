ae537590fbbd0a29f62dbc3bb899093e1ff273c8
milosz08 <gilgamilosz451@gmail.com>
2024-05-11 01:12:16 +0200

    add missing path property and minify html in production build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 import { resolve } from 'path';
 import { URL } from 'url';
 import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 
 export default () => {
@@ -29,6 +30,9 @@ export default () => {
             ],
           ],
         },
+      }),
+      createHtmlPlugin({
+        minify: true,
       }),
     ],
     resolve: {
