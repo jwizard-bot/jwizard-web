25b8a7e674ced9418a124c3b795d381c9bb475f2
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-25 18:55:58 +0200

    minor configuration changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,34 +3,12 @@
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
 
-const { MODE, API_URL, INVITE_LINK, ORG_LINK, BUILD_VERSION } = window.jwizard;
-
-type ConfigTypes = {
-  isProdMode: boolean;
-  apiUrl: string;
-  inviteLink: string;
-  orgLink: string;
-  buildVersion: string;
-};
-
-declare global {
-  interface Window {
-    jwizard: {
-      MODE: string;
-      API_URL: string;
-      INVITE_LINK: string;
-      ORG_LINK: string;
-      BUILD_VERSION: string;
-    };
-  }
-}
-
-const config: ConfigTypes = {
-  isProdMode: MODE === 'production',
-  apiUrl: API_URL,
-  inviteLink: INVITE_LINK,
-  orgLink: ORG_LINK,
-  buildVersion: BUILD_VERSION,
+const config = {
+  isProdMode: import.meta.env.MODE === 'production',
+  apiUrl: import.meta.env.API_URL,
+  signalApiUrl: import.meta.env.SIGNAL_API_URL,
+  orgLink: import.meta.env.ORG_LINK,
+  buildVersion: import.meta.env.BUILD_VERSION,
 };
 
 export default config;
