76b3d29e483ed84dfd166dde3f128d94b919cf35
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-22 21:25:48 +0100

    (fix) export tailwind custom colors object

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -3,6 +3,49 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 export const baseContent = ['./src/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'];
 
+export const colors = {
+  white: 'hsl(var(--white))',
+  black: 'hsl(var(--black))',
+  border: 'hsl(var(--border))',
+  input: 'hsl(var(--input))',
+  ring: 'hsl(var(--ring))',
+  background: 'hsl(var(--background))',
+  foreground: 'hsl(var(--foreground))',
+  primary: {
+    DEFAULT: 'hsl(var(--primary))',
+    foreground: 'hsl(var(--primary-foreground))',
+    success: 'hsl(var(--primary-success))',
+    warning: 'hsl(var(--primary-warning))',
+    danger: 'hsl(var(--primary-danger))',
+  },
+  secondary: {
+    DEFAULT: 'hsl(var(--secondary))',
+    foreground: 'hsl(var(--secondary-foreground))',
+  },
+  destructive: {
+    DEFAULT: 'hsl(var(--destructive))',
+    foreground: 'hsl(var(--destructive-foreground))',
+  },
+  muted: {
+    DEFAULT: 'hsl(var(--muted))',
+    foreground: 'hsl(var(--muted-foreground))',
+    'light-foreground': 'hsl(var(--muted-light-foreground))',
+    'dark-foreground': 'hsl(var(--muted-dark-foreground))',
+  },
+  accent: {
+    DEFAULT: 'hsl(var(--accent))',
+    foreground: 'hsl(var(--accent-foreground))',
+  },
+  popover: {
+    DEFAULT: 'hsl(var(--popover))',
+    foreground: 'hsl(var(--popover-foreground))',
+  },
+  card: {
+    DEFAULT: 'hsl(var(--card))',
+    foreground: 'hsl(var(--card-foreground))',
+  },
+} as const;
+
 export const baseConfig: Omit<Config, 'content'> = {
   darkMode: ['class'],
   safelist: ['dark'],
@@ -13,44 +56,7 @@ export const baseConfig: Omit<Config, 'content'> = {
         logo: ['var(--font-logo)'],
         pixelated: ['var(--font-pixelated)'],
       },
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-          success: 'hsl(var(--primary-success))',
-          warning: 'hsl(var(--primary-warning))',
-          danger: 'hsl(var(--primary-danger))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-      },
+      colors,
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
--- packages/ui/src/resolved-config.ts
+++ packages/ui/src/resolved-config.ts
@@ -1,7 +1,7 @@
-import { baseConfig } from '@jwizard-web/tailwind-config/tailwind';
+import { baseConfig, colors } from '@jwizard-web/tailwind-config/tailwind';
 import type { Config } from 'tailwindcss';
 import resolveConfig from 'tailwindcss/resolveConfig';
 
 const fullConfig = resolveConfig(baseConfig as Config);
 
-export default fullConfig;
+export { fullConfig, colors };
