33c70ee43f7e561b53b2fef8520915d6792a47da
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-26 06:39:10 +0100

    (fix) add loading screen to status subpages

---
+++ apps/landing-page/src/app/status/loading.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { PageSpinner } from '@jwizard-web/ui/component/page-spinner';
+
+const Loading: React.FC = (): React.ReactElement => <PageSpinner />;
+
+export default Loading;
